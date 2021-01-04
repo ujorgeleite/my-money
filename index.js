fe3c2bea8a8ae5d2f791a68d23a2711278bde7f4
Jorge Fernando Damasio Leite <jorge.leite@hypeflame.com.br>
2021-01-04 12:04:46 -0300

    refactor(main): adjust import file logic

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const rodar = async () => {
         console.log(`Passou: ${tempo} segundos`)
         tempo += 1
     }, 1000)
-    const multipolicadorTempoExecucao = (fileNames.length / 3)
+    const multipolicadorTempoExecucao = (fileNames.length / 4)
     const tempoDeExecucao = multipolicadorTempoExecucao * 1000
     console.log(`A execução do arquivos vai começar em ${multipolicadorTempoExecucao} segundos, aguarde....`)
 
@@ -22,8 +22,9 @@ const rodar = async () => {
 
             const arquivo = await file.getFile(name)
 
-            if (!!arquivo.errors) {
-                console.log(`Erro ao tentar ler o arquivo ${name}`)
+            if (!!arquivo.errors && arquivo.errors.length > 0) {
+                console.log(`Erro ao tentar ler o arquivo ${arquivo.errors}`)
+                console.log(`Erros ${name}`)
             } else {
 
                 console.log('leu o arquivo => ', name)
